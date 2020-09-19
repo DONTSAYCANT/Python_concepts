9ce277199b38f4a3b1054a1d9c915c32fae15c73
PARTHIPAN KANNAN <66922169+DONTSAYCANT@users.noreply.github.com>
2020-09-19 21:42:29 +0530

    Modules

---
+++ calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def add_mul_ret(x,y):
+    return (x+y) * (x-y)
+
+def par(x):
+    return x ** 0.5
+
+def spider(x,y,z):
+    return x * y * z
+
+
+def cric(x,y,z,e,r):
+    return 10
+
+# In[ ]:
+
+
+
+
---
+++ main.py
@@ -0,0 +1,7 @@
+import calculator
+
+
+print(calculator.par(5))
+
+print(calculator.cric(10,2,3,4,5))
+
